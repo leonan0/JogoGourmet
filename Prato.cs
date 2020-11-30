976569ede784e33c3eb1266de0a32ce4ce061e61
Leonan Ferreira <leonanf371@gmail.com>
2020-11-30 14:11:47 -0300

    Add files via upload

---
+++ Prato.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoGourmet
+{
+    public class Prato
+    {
+        public string Nome { get; set; }
+        
+
+        public Prato(string nome)
+        {
+            this.Nome = nome;
+        }
+
+        public string GetNome()
+        {
+            return this.Nome;
+        }
+    }
+}
+
---
+++ PratosCaracteristicas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoGourmet
+{
+    public class PratosCaracteristicas
+    {
+        public Caracteristica Caracteristica { get; set; }
+
+        public Prato Prato { get; set; }
+
+        public PratosCaracteristicas(Prato prato, Caracteristica caracteristica)
+        {
+            this.Caracteristica = caracteristica;
+            this.Prato = prato;
+        }
+    }
+}
