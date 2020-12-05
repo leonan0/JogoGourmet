257e8a56c7bcf80dbb652bc31e8f35efac63c20e
leonan <leonanf371@gmail.com>
2020-12-05 08:05:06 -0300

    Revisado

---
+++ JogoGourmet/Prato.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JogoGourmet
+{
+    public class Prato
+    {
+        public string Nome { get; set; }
+        public string Caracateristica { get; set; }
+
+        public Prato(string nome, string caracteristica)
+        {
+            this.Nome = nome;
+            this.Caracateristica = caracteristica;
+
+        }
+
+        public string GetNome()
+        {
+            return this.Nome;
+        }
+
+        public string GetCaracteristica()
+        {
+
+            return this.Caracateristica;
+
+        }
+
+    }
+}
